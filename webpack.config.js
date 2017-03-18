7c0da689004cea0a106d6a7dc881196e271408c8
Henry Cruz <ihenvyr@gmail.com>
2017-03-18 15:24:06 +0800

    bundle.js added to docs, cross-env added as dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development')
+      "process.env": {
+        "NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
       }
     })
   ],
